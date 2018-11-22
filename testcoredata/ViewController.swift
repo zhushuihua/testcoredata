b20b1edd1e86e9e9cb8e08629ae407302c91eaa1
Joseph <null>
2018-11-22 11:21:19 +0800

    1. embed view controller in navigation view controller 2. add save to core data function 3. add barbuTton to add item to save to core date

--- testcoredata/ViewController.swift
+++ testcoredata/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        var entry:UITextField!
+        let alert = UIAlertController(title: "Add Item To Core Data", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Key In New Item Here"
+            entry = textField
+        }
+        let addAction = UIAlertAction(title: "Add", style: .default) {
+            action in
+            let item = Item(context: self.context)
+            item.title = entry.text!
+            item.done = false
+            try? self.context.save()
+        }
+        alert.addAction(addAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
