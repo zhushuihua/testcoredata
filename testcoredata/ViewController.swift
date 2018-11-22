d8aaa36c79ce97caaee8e171f067f5ea897d10df
Joseph <null>
2018-11-22 10:46:21 +0800

    Initial Commit

---
+++ testcoredata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testcoredata
+//
+//  Created by Shuihua Zhu on 22/11/18.
+//  Copyright © 2018 UMA Mental Arithmetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
